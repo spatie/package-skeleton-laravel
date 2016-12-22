c024a700d66da7c4740785a2decae32b83799274
Gijs Jorissen <gizburdt@users.noreply.github.com>
2016-12-22 23:46:12 +0100

    StyleCI

--- src/SkeletonServiceProvider.php
+++ src/SkeletonServiceProvider.php
@@ -12,17 +12,17 @@ class SkeletonServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-           $this->publishes([
-               __DIR__.'/../config/skeleton.php' => config_path('skeleton.php'),
-           ], 'config');
+            $this->publishes([
+                __DIR__.'/../config/skeleton.php' => config_path('skeleton.php'),
+            ], 'config');
 
-           /*
-           $this->loadViewsFrom(__DIR__.'/../resources/views', 'skeleton');
+            /*
+            $this->loadViewsFrom(__DIR__.'/../resources/views', 'skeleton');
    
-           $this->publishes([
-               __DIR__.'/../resources/views' => base_path('resources/views/vendor/skeleton'),
-           ], 'views');
-           */
+            $this->publishes([
+                __DIR__.'/../resources/views' => base_path('resources/views/vendor/skeleton'),
+            ], 'views');
+            */
         }
     }
 
