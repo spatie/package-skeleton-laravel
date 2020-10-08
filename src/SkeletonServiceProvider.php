b5beac71be0cc9b2a51af2276f6818e78ab9f780
Fabrice Yopa <fabrice_yopa@yahoo.fr>
2020-10-08 20:45:23 +0100

    change with static call

--- src/SkeletonServiceProvider.php
+++ src/SkeletonServiceProvider.php
@@ -19,7 +19,7 @@ class SkeletonServiceProvider extends ServiceProvider
             ], 'views');
 
             $migrationFileName = 'create_skeleton_table.php';
-            if (! $this->migrationFileExists($migrationFileName)) {
+            if (! self::migrationFileExists($migrationFileName)) {
                 $this->publishes([
                     __DIR__ . "/../database/migrations/{$migrationFileName}.stub" => database_path('migrations/' . date('Y_m_d_His', time()) . '_' . $migrationFileName),
                 ], 'migrations');
