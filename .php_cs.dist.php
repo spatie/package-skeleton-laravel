32bc16381132555660a72b890fe80207c2f62f1e
Niels <niels@spatie.be>
2021-05-19 08:56:31 +0200

    add class_attributes_separation to php cs config

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -26,6 +26,11 @@ return (new PhpCsFixer\Config())
         ],
         'phpdoc_single_line_var_spacing' => true,
         'phpdoc_var_without_name' => true,
+        'class_attributes_separation' => [
+            'elements' => [
+                'method' => 'one',
+            ],
+        ],
         'method_argument_space' => [
             'on_multiline' => 'ensure_fully_multiline',
             'keep_multiple_spaces_after_comma' => true,
