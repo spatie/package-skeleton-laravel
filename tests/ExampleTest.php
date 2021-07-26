b56d5e8bcc945c2f48e07cb5a7e93796f2120654
abenettt <me@abenet.email>
2021-07-26 12:54:21 +0200

    Add phpstan/larastan

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -5,7 +5,7 @@ namespace VendorName\Skeleton\Tests;
 class ExampleTest extends TestCase
 {
     /** @test */
-    public function true_is_true()
+    public function true_is_true(): void
     {
         $this->assertTrue(true);
     }
