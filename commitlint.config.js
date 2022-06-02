a7a35649b0affa515801ff88634c46e0cf57fd04
Silvio Iannone <silvio@grebban.com>
2022-06-02 11:28:50 +0200

    feat: Add PHP_CodeSniffer and commit linting

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'subject-case': [
+            2,
+            'always',
+            ['sentence-case'],
+        ]
+    }
+}
