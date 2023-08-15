1c2a5720df48bee1a8affb427ef4cd0ef58030d4
Mior Muhammad Zaki <crynobone@gmail.com>
2023-08-15 19:29:56 +0800

    Support Workbench
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ workbench/app/Providers/WorkbenchServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Workbench\App\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class WorkbenchServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Route::view('/', 'welcome');
+    }
+}
