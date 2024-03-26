c4d640f83316ff141ba0f3e31c6205c10ea2336a
JackyChen <zhangqi.chen.cn@gmail.com>
2024-03-26 22:49:43 +0800

    Add return type declaration to function

--- src/Facades/Skeleton.php
+++ src/Facades/Skeleton.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Facade;
  */
 class Skeleton extends Facade
 {
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return \VendorName\Skeleton\Skeleton::class;
     }
