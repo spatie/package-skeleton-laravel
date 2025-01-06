78d3446c86eff5c807bc3407ef81508b78a15d04
freekmurze <freekmurze@users.noreply.github.com>
2025-01-06 09:43:49 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -27,11 +27,11 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-		
-		/*
-		 foreach (\Illuminate\Support\Facades\File::allFiles(__DIR__ . '/database/migrations') as $migration) {
-		    (include $migration->getRealPath())->up();
-		 }
-		 */
+
+        /*
+         foreach (\Illuminate\Support\Facades\File::allFiles(__DIR__ . '/database/migrations') as $migration) {
+            (include $migration->getRealPath())->up();
+         }
+         */
     }
 }
