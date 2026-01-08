4ea2203a0658bf20f60d498c320901e35c46cf9d
Mzati Tembo <106585054+Mzati1@users.noreply.github.com>
2026-01-08 21:33:05 +0200

    refactor: update namespace from Mzati\\Paychangu to Paychangu\\Laravel
    Update all namespace references in composer.json and TestCase.php to reflect the new package naming convention

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,7 +13,7 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Mzati\\Paychangu\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn (string $modelName) => 'Paychangu\\Laravel\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
     }
 
